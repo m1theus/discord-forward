d9350b3345e3e5f6929c7cfaeede3a0f20cfc2ef
Matheus Martins <matheus.math27@hotmail.com>
2022-10-24 17:46:27 -0300

    Add mongodb to save forwarded messages

--- src/index.js
+++ src/index.js
@@ -3,25 +3,32 @@ dotenv.config();
 const _ = require('lodash');
 const dayjs = require('dayjs')
 const DiscordClient = require('./discord')
+const { MongoClient } = require('mongodb');
 
 dayjs.locale('en')
 
-const { CHANNEL_FROM, CHANNEL_TO, CLIENT_PUBLIC_KEY } = process.env
+const { CHANNEL_FROM, CHANNEL_TO, CLIENT_PUBLIC_KEY, MONGO_URI } = process.env
 
 const client = new DiscordClient(CLIENT_PUBLIC_KEY);
-
-const PROCESSED_ID = [];
+const mongodb = new MongoClient(MONGO_URI);
 
 (async () => {
   async function forwardMessage(messages) {
+    const db = mongodb.db('discord');
+    const collection = db.collection('discord_forward');
     const processed = [];
     for (const msg of messages) {
-      const hasMessage = _.find(PROCESSED_ID, (id) => id === msg.id);
-
+      const hasMessage = await collection.findOne({ _id: msg.id })
+      console.log('has_message', !!hasMessage)
       try {
         if (!hasMessage) {
           await client.sendMessage(CHANNEL_TO, msg);
-          processed.push(msg)
+          console.log('saving...')
+          await collection.insertOne({
+            _id: msg.id,
+            ...msg,
+          });
+          processed.push(msg);
           console.log('success_send_message =>', msg.id);
         }
       } catch (error) {
@@ -33,10 +40,9 @@ const PROCESSED_ID = [];
 
   const pooling = async function () {
     const messages = await client.getMessages(CHANNEL_FROM, 20);
-    console.log(messages)
     await forwardMessage(messages);
   }
 
   await pooling();
-  setInterval(pooling, 1000);
+  setInterval(pooling, 8000);
 })();
